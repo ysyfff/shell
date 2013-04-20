1216ffd16469fa7bca8297882d87d31a670ad301
ysyfff <1156546473@qq.com>
2013-04-21 03:11:56 +0800

    wrong declare

--- declares.sh
+++ declares.sh
@@ -12,12 +12,12 @@ fi
 
 declare -i date_dem='date --date="$date2" +%s'
 declare -i date_now="date +%s"
-declare -i date_diff=$( ($date_dem-$date_now) )
-declare -i date_d=$( ($date_diff/60/60/24) )
+declare -i date_diff=$(($date_dem-$date_now))
+declare -i date_d=$(($date_diff/60/60/24))
 
 if [ "$date_diff" -lt "0" ]; then
-    echo "You had been demobilization before: " $( (-1*#date_d) ) " ago"
+    echo "You had been demobilization before: " $((-1*#date_d)) " ago"
 else
-    declare -i date_h=$( ( $( ($date_diff-$date_d*60*60*24) ) /60/60 ) )
+    declare -i date_h=$(($(($date_diff-$date_d*60*60*24))/60/60))
     echo "You will demobilization after $date_d days and #date_h hours"
 fi
